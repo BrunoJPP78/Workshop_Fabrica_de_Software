d47a12f8314f82c0ac1bff814886af4c6814a005
BrunoJPP78 <brunojpoker@gmail.com>
2023-03-31 16:35:55 -0300

    Dia 4 - Workshop Fábrica de Software

---
+++ setup/urls.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+from setup.views import ToDoViewSet
+
+router = routers.DefaultRouter()
+router.register(r'api', ToDoViewSet)
+
+urlpatterns = router.urls
+
---
+++ setup/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from setup.serializers import ToDoSerializer
+from setup.models import ToDo
+
+
+
+class ToDoViewSet(viewsets.ModelViewSet):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
